0dc9902cd20e160b0ef1dd3af81a9e7fce213f50
Md Ramzan Ali <mdramzanali823@gmail.com>
2026-06-06 23:43:39 +0600

    delete the testing

--- LeetCode/00-testing/testingForPushingInGitHub.java
+++
@@ -1,7 +0,0 @@
-public class testingForPushingInGitHub {
-    public static void main(String[] args) {
-        for(int i = 0; i< 5 ; i++){
-            System.out.println("I'm the next ICPC Winner.");
-        }
-    }
-}
