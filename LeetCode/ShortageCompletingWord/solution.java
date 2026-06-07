a1737aea87fa849a1ff801046d04dd02233cf049
Md Ramzan Ali <mdramzanali823@gmail.com>
2026-06-07 17:01:21 +0600

    adding shortest completing word problem's solution

---
+++ LeetCode/ShortageCompletingWord/solution.java
@@ -0,0 +1,34 @@
+package ShortageCompletingWord;
+
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        int [] count = new int [26];
+        for(char ch : licensePlate.toLowerCase().toCharArray()){
+            if(Character.isLetter(ch)){
+                count[ch-'a']++;
+            }
+        }
+        String result = "";
+        for(String word : words){
+            if(!result.equals("") && word.length()>=result.length()){
+                continue;
+            }
+            if(isCompleteWord(word, count)){
+                result = word;
+            }
+        }
+        return result;
+    }
+    private boolean isCompleteWord(String word, int[] targetCounts) {
+        int[] wordCounts = new int[26];
+        for (char c : word.toCharArray()) {
+            wordCounts[c - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (wordCounts[i] < targetCounts[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
